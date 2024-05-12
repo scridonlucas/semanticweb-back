ab53bab5011181267667662a91cb356aff1afbb5
scridonlucas <lukas_scridon@yahoo.com>
2024-05-12 22:27:00 +0300

    serving static files, also creating route for http request

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import scrapeRouter from './routes/scrape.js';
+import path from 'path';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
 
 const app = express();
+
 app.use(cors());
+
+app.get('/json-ld/', (req, res) => {
+  const rootPath = process.cwd();
+  res.sendFile(path.join(rootPath, 'jsonLdHtmlPage', 'index.html'));
+});
+
 app.get('/', (req, res) => {
   res.send('Proiect Web Semantic - Scridon Lucas, Timandi Sabin');
 });
